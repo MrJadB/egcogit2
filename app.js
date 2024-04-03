6142a5b311706555fa7dc2d8fb0e0fd5d894017d
MrJadB <act21850class8@gmail.com>
2024-04-03 15:37:34 +0700

    new web test

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World Here are the update version'))
+
+app.listen(3000, () => console.log('server start'))
